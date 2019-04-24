2e6a3fa24a4beeef5a5d34f6727e9d3784d386a2
hameedimran <hameedimran@gmail.com>
2019-04-24 00:19:39 -0500

    Initial commit

---
+++ src/main/java/com/glic/droolsvalidate/App.java
@@ -0,0 +1,69 @@
+package com.glic.droolsvalidate;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.drools.decisiontable.InputType;
+import org.drools.decisiontable.SpreadsheetCompiler;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.event.rule.DebugAgendaEventListener;
+import org.kie.api.event.rule.DebugRuleRuntimeEventListener;
+import org.kie.api.io.Resource;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import com.glic.droolsvalidate.model.Applicant;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) throws FileNotFoundException {
+
+		/*SpreadsheetCompiler converter = new SpreadsheetCompiler();
+		final String drl = converter.compile(
+				new FileInputStream(new File(
+						"E:\\programmertechExamples\\JEE\\DroolsDecisionTable\\drooolsValidate\\src\\main\\resources\\statefulPkg\\ValidateApplicant.xlsx")),
+				InputType.XLS);
+		System.out.println(drl);*/
+		Set<Applicant> applicants = new HashSet<>();
+		applicants.add(new Applicant(1, "Raj", "25/05/1975", "M"));
+		applicants.add(new Applicant(2, "Ram", "25/03/1905", "M"));
+		applicants.add(new Applicant(3, "Anjali", "25/03/1993", "F"));
+		applicants.add(new Applicant(4, "Rani", "01/07/1945", "F"));
+		executeStatefull(applicants);
+
+		for (Applicant applicant : applicants) {
+			System.out.println(applicant.toString());
+		}
+
+	}
+
+	public static void executeStatefull(Set<Applicant> applicants) {
+		try {
+			// load up the knowledge base
+			KieServices ks = KieServices.Factory.get();
+			KieContainer kContainer = ks.getKieClasspathContainer();
+			KieBase kBase = kContainer.getKieBase("stateful");
+			KieSession kSession = kBase.newKieSession();
+			/*kSession.addEventListener(new DebugAgendaEventListener());
+			kSession.addEventListener(new DebugRuleRuntimeEventListener());*/
+			for (Applicant applicant : applicants) {
+				kSession.insert(applicant);
+				kSession.fireAllRules();
+			}
+			
+			kSession.dispose();
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/com/glic/droolsvalidate/model/Applicant.java
@@ -0,0 +1,90 @@
+package com.glic.droolsvalidate.model;
+
+public class Applicant {
+
+	private Integer applicantId;
+	private String name;
+	private String dob;
+	private String gender;
+	private boolean validApplicant;
+	private String applicantType;
+	private Double accountOpenAmt;
+
+	public Applicant() {
+
+	}
+
+	public Applicant(Integer applicantId, String name, String dob, String gender) {
+		super();
+		this.applicantId = applicantId;
+		this.name = name;
+		this.dob = dob;
+		this.gender = gender;
+	}
+
+	public Integer getApplicantId() {
+		return applicantId;
+	}
+
+	public void setApplicantId(Integer applicantId) {
+		this.applicantId = applicantId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public boolean isValidApplicant() {
+		return validApplicant;
+	}
+
+	public void setValidApplicant(boolean validApplicant) {
+		this.validApplicant = validApplicant;
+	}
+
+	public String getApplicantType() {
+		return applicantType;
+	}
+
+	public void setApplicantType(String applicantType) {
+		this.applicantType = applicantType;
+	}
+	
+	public Double getAccountOpenAmt() {
+		return accountOpenAmt;
+	}
+
+	public void setAccountOpenAmt(Double accountOpenAmt) {
+		this.accountOpenAmt = accountOpenAmt;
+	}
+
+	@Override
+	public String toString() {
+		return "Applicant [applicantId=" + applicantId + ", name=" + name + ", dob=" + dob + ", gender=" + gender
+				+ ", validApplicant=" + validApplicant + ", applicantType=" + applicantType + ", accountOpenAmt="
+				+ accountOpenAmt + "]";
+	}
+
+	
+
+}
